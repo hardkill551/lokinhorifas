6e6b95be7d4919d1d8cf54304f238ab73432c054
Hugo Rodrigues Costa <hugo.rc@dinfor.uniceub>
2024-08-06 08:59:53 -0300

    feat/postSkin

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -1,8 +1,53 @@
-import style from './live.module.css';
-import Image from 'next/image';
+import React, { useContext, useEffect, useState } from "react";
+import style from "./styles/Twitch.module.css";
+import Image from "next/image";
+import Background from "@/images/background.png";
+import logovermelho from "@/images/logovermelho.png";
+import { TwitchEmbed } from "react-twitch-embed";
+import ReactTwitchEmbedVideo from "react-twitch-embed-video";
+import { TextContext } from "../../utils/contextText";
+import TextContextType from "../../utils/interfaces";
+import axios from "axios";
+
+const Twitch = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const { textInfo, setTextInfo } = useContext(TextContext) as TextContextType;
+  useEffect(() => {
+    axios.get(process.env.NEXT_PUBLIC_REACT_NEXT_APP + "/text").then((res: any) => {
+      console.log(res.data);
+      setTextInfo(res.data);
+    }).catch((err: any) => {
+      console.error(err.response ? err.response.data : 'Erro ao buscar dados');
+    });
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 950);
+    };
+
+    handleResize(); // Chama a função imediatamente para definir o estado inicial
+
+    window.addEventListener("resize", handleResize);
+
+    setIsLoading(false); // Marca que o carregamento foi concluído após a montagem inicial
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Renderiza a live dependendo do tamanho da tela e do estado de carregamento
+  const renderLive = () => {
+    if (isMobile) {
+      return <div className={style.Live}>
+        <ReactTwitchEmbedVideo channel="evandro_vidal" />
+      </div>;
+    } else {
+      return <TwitchEmbed className={style.Live} channel="Gaules" />;
+    }
+  };
 
 
-const Live = () => {
+
   return (
     <div className={style.container}>
       <Image
@@ -23,7 +68,7 @@ const Live = () => {
         {!isLoading && renderLive()}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Live
+export default Twitch;
