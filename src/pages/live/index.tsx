62a7ea824800783e1a74f8ea49ac46a52e764198
Leandro Ferreira <leandro.fcode@gmail.com>
2024-08-08 19:17:24 -0300

    add style live & fix minor errors

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -1,60 +1,48 @@
 import React, { useContext, useEffect, useState } from "react";
 import style from './live.module.css';
-import Image from "next/image";
 import { TwitchEmbed } from "react-twitch-embed";
-import ReactTwitchEmbedVideo from "react-twitch-embed-video";
 import TextContextType from "../../utils/interfaces";
 import axios from "axios";
 import { TextContext } from "contexts/TextContext";
 
 const Twitch = () => {
-  const [isMobile, setIsMobile] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [ videoWidth, setVideoWidth ] = useState(0)
   const { textInfo, setTextInfo } = useContext(TextContext) as TextContextType;
+
   useEffect(() => {
     axios.get(process.env.NEXT_PUBLIC_REACT_NEXT_APP + "/text").then((res: any) => {
-      console.log(res.data);
       setTextInfo(res.data);
     }).catch((err: any) => {
       console.error(err.response ? err.response.data : 'Erro ao buscar dados');
     });
+
+    
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 950);
+      const isBigger = Number(window.innerWidth) > 1110 ? 1110 : Number(window.innerWidth - 200)
+      setVideoWidth(isBigger)
+      setIsLoading(true);
     };
 
     handleResize(); // Chama a função imediatamente para definir o estado inicial
 
     window.addEventListener("resize", handleResize);
 
-    setIsLoading(false); // Marca que o carregamento foi concluído após a montagem inicial
-
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
-  // Renderiza a live dependendo do tamanho da tela e do estado de carregamento
-  const renderLive = () => {
-    if (isMobile) {
-      return <div className={style.Live}>
-        <ReactTwitchEmbedVideo channel="evandro_vidal" />
-      </div>;
-    } else {
-      return <TwitchEmbed className={style.Live} channel="Gaules" />;
-    }
-  };
-
 
 
   return (
-    <div className={style.container}>   
-      <main className={style.conteudo}>
-
-        <h1 className={style.tituloLive}>{textInfo.text}</h1>
-
-        {/* Renderiza a live apenas quando o carregamento estiver concluído */}
-        {!isLoading && renderLive()}
-      </main>
+    <div className={style.live}>
+      <div className={style.liveWrapper}>
+        <h1>{isLoading && textInfo.text}</h1>
+        <div className={style.liveFeed}>
+          <TwitchEmbed channel="evandro_vidal" width={videoWidth} height={700} />
+        </div>
+      </div>
     </div>
   );
 };
