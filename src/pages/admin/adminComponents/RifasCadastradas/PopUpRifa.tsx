415aad27e480bb96921fdae65674b21460d0e158
lucas araujo <lucasao744@gmail.com>
2024-08-22 17:23:32 -0300

    PopUpRifa

--- src/pages/admin/adminComponents/RifasCadastradas/PopUpRifa.tsx
+++ src/pages/admin/adminComponents/RifasCadastradas/PopUpRifa.tsx
@@ -1,14 +1,27 @@
 import style from "./RifasCadastradas.module.css";
 
-export default function PopUpRifa() {
+export default function PopUpRifa({setPopUpRifaRifa}:any) {
 
     return (
         <div className={style.ContainerPopUpRifa}>
             <div className={style.ContentPopUpRifa}>
                 <div className={style.ButtonExitUpdate}>
-                    <button onClick={() => (false)} className={style.ButtonExitUpdateStyle}>x</button>
+                    <button onClick={() => setPopUpRifaRifa(false)} className={style.ButtonExitUpdateStyle}>x</button>
                 </div>
-                <p>skjnfaksdjlnfj</p>
+                <div>
+                    <p className={style.TitleDescriptionPopUpRifa}>Quantida de Participantes: <span>3</span></p>
+                   <p className={style.TitleDescriptionPopUpRifa}>Limite de Participante: <span>20</span></p>
+                   <p className={style.TitleDescriptionPopUpRifa}>Estado: <span className={style.EstateRaffle}>Em analise</span></p>
+                   <p className={style.TitleDescriptionPopUpRifa}>Valor Total: <span>R$:500</span> </p>
+                   <p className={style.TitleDescriptionPopUpRifa}>Limite de valor por rifa: <span>R$:50</span></p>
+                   <p className={style.TitleDescriptionPopUpRifa}>Criado em: <span>12/08/2024</span></p>
+                   <p className={style.TitleDescriptionPopUpRifa}>Modificada em: <span>12/09/2024</span></p>
+                </div>
+                <div>
+                    <button className={style.ButtonPopUpRifa}>Ativar Rifa</button>
+                   <button className={style.ButtonPopUpRifa}>Deletar Rifa</button>
+                   <button className={style.ButtonPopUpRifa}>Atualizar Rifa</button>
+                </div>
             </div>
         </div>
     );
